480e69a5750fd07e069272abc109cbdff0e00560
cphaneuf <cphaneuf@umich.edu>
2020-07-26 14:08:35 -0400

    adding timestamp to data file name.

--- CommandLineClassifier/CLC.cpp
+++ CommandLineClassifier/CLC.cpp
@@ -11,6 +11,7 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <ctime>
 using namespace std;
 
 struct subject {
@@ -33,21 +34,21 @@ struct subject {
 
 class completeData {
 public:
-    
+
     ////////////////////////////////////
     /////////// DATA MEMBERS ///////////
     ////////////////////////////////////
-    
+
     unordered_map<string, subject> subjects;
     vector<char> sessions;
     vector<string> responses;
-    
+
     ////////////////////////////////////
     ///////// MEMBER FUNCTIONS /////////
     ////////////////////////////////////
-    
+
     /// Functions to Store Responses ///
-    
+
     int store_responses(ifstream &responses, char &letter, int num_s, int num_q) {
         // Experimenter was prompting (do not store)
         if (letter != 'A' && letter != 'B' && letter != 'C' &&
@@ -56,11 +57,11 @@ public:
             getline(responses, reprompt);
             return 0;
         }
-        
+
         string response = "";
         getline(responses, response);
         string fullID = to_string(num_s) + letter;
-        
+
         // If subject key does not exist in the hash table, declare and add a new subject...
         if (subjects.find(fullID) == subjects.end()) {
             subject temp;
@@ -99,55 +100,55 @@ public:
                 subjects[fullID].q5.append(response);
             }
         }
-        
+
         return 0;
     } // store_responses()
-    
+
     int process_question(ifstream &text, int session_number, int question_number) {
         string first = "";
         text >> first;
-        
+
         while (first != "QUESTION:" && first != "VIDEO:" && first != "EOF") {
             char last_char = first[first.length() - 2];
             store_responses(text, last_char, session_number, question_number);
             text >> first;
         }
-        
+
         return 0;
     } // process_question()
-    
+
     int process_session() {
         cout << "Please type the name of the file you wish to process, then press 'return' or 'enter' after responding:\n";
         string filename = "";
         cin >> filename;
-        
+
         ifstream file;
         file.open(filename);
-        
+
         if (file.is_open()) {
             int num_sessions = 0;
             file >> num_sessions;
             string rem_txt = "";
             getline(file, rem_txt);
-            
+
             for (int i = 0; i < num_sessions; ++i) {
                 // Get the video label
                 string vid = "";
                 file.ignore();
                 getline(file, vid);
-                
+
                 // Get the session number
                 string sess = "";
                 file >> sess;
                 int sess_num = 0;
                 file >> sess_num;
-                
+
                 // Get the experimental condition
                 string tp = "";
                 file >> tp;
                 string cond = "";
                 file >> cond;
-                
+
                 // Store the experimental condition (control, test)
                 if (cond[0] == 'C') {
                     sessions.push_back('C');
@@ -155,12 +156,12 @@ public:
                 else {
                     sessions.push_back('T');
                 }
-                
+
                 // Get the coder information
                 string cods = "";
                 file.ignore();
                 getline(file, cods);
-                
+
                 // Base decisions off of experimental condition (if control, else test)
                 if (cond[0] == 'C') {
                     string q = "";
@@ -195,14 +196,14 @@ public:
             }
             file.close();
         }
-        
+
         else {
             cout << "ABORT: Unable to open file.";
         }
-        
+
         return 0;
     } // process_session()
-    
+
     ///// User Interface Functions /////
 
     int print_prompt(string sub_number, char question_number) {
@@ -214,7 +215,7 @@ public:
             cout << "(0 = NO, 1 = YES, 2 = UNCLEAR)\n";
             cout << "Please press 'return' or 'enter' after responding.\n";
         }
-        
+
         else if (question_number == '2') {
             cout << "Q: " << "Question 2 body, part A.\n";
             cout << "A: " << subjects[sub_number].q2 << "\n";
@@ -223,7 +224,7 @@ public:
             cout << "(0 = NO, 1 = YES, 2 = UNCLEAR)\n";
             cout << "Please press 'return' or 'enter' after responding.\n";
         }
-        
+
         else if (question_number == '3') {
             cout << "Q: " << "Question 3 body, part A.\n";
             cout << "A: " << subjects[sub_number].q3 << "\n";
@@ -232,7 +233,7 @@ public:
             cout << "(0 = NO, 1 = YES, 2 = UNCLEAR)\n";
             cout << "Please press 'return' or 'enter' after responding.\n";
         }
-        
+
         else if (question_number == '4') {
             cout << "Q: " << "Question 4 body, part A.\n";
             cout << "A: " << subjects[sub_number].q4 << "\n";
@@ -241,7 +242,7 @@ public:
             cout << "(0 = NO, 1 = YES, 2 = UNCLEAR)\n";
             cout << "Please press 'return' or 'enter' after responding.\n";
         }
-        
+
         else if (question_number == '5') {
             cout << "Q: " << "Question 5 body, part A.\n";
             cout << "A: " << subjects[sub_number].q5 << "\n";
@@ -250,10 +251,10 @@ public:
             cout << "(0 = NO, 1 = YES, 2 = UNCLEAR)\n";
             cout << "Please press 'return' or 'enter' after responding.\n";
         }
-        
+
         return 0;
     } // print_prompt()
-    
+
     int process_choice() {
         int max_it = int(responses.size());
         for (int i = 0; i < max_it; ++i) {
@@ -261,15 +262,15 @@ public:
             int rand_num = 0;
             if (max != 0) {
                 rand_num = rand() % max;
-            } 
+            }
             string rand_str = responses[rand_num];
             string sub_lookup = rand_str.substr(0, 2);
             char q_lookup = rand_str[2];
-            
+
             // Use sub_lookup and q_lookup to find the answer and print out the prompt
             cout << "\n\n";
             print_prompt(sub_lookup, q_lookup);
-            
+
             // Get the user's choice and store their response
             while (true) {
                 string choice = "";
@@ -339,53 +340,53 @@ public:
             // Remove item from list since the response has been coded
             responses.erase(responses.begin() + rand_num);
         }
-        
+
         return 0;
     } // process_choice()
-    
+
     int make_response_list() {
         // Iterate through subjects to generate a list of all responses; for each question a subject answered, add it to the total list of responses
         for (pair<string, subject> sub : subjects) {
-            // Q1 
+            // Q1
 	    if (!sub.second.q1.empty()) {
                 string code = sub.first + "1";
                 responses.push_back(code);
             }
-            
+
 	    // Q2
             if (!sub.second.q2.empty()) {
                 string code = sub.first + "2";
                 responses.push_back(code);
             }
-            
+
 	    // Q3
             if (!sub.second.q3.empty()) {
                 string code = sub.first + "3";
                 responses.push_back(code);
             }
-            
+
 	    // Q4
             if (!sub.second.q4.empty()) {
                 string code = sub.first + "4";
                 responses.push_back(code);
             }
-            
+
 	    // Q5
             if (!sub.second.q5.empty()) {
                 string code = sub.first + "5";
                 responses.push_back(code);
             }
         }
-        
+
         // Once all responses are compiled, randomly select an item from the list and remove it
         process_choice();
-        
+
         return 0;
     } // make_response_list()
-    
+
     int print() {
         cout << "TESTING THE TERMAINAL APPLICATION:\n";
-        
+
         // SESSION 1 - responses
         cout << subjects["1A"].q1 << endl;
         cout << subjects["1B"].q1 << endl;
@@ -417,7 +418,7 @@ public:
         cout << subjects["1D"].q5 << endl;
         cout << subjects["1E"].q5 << endl;
         cout << subjects["1F"].q5 << endl;
-        
+
         // SESSION 1 - coding
         cout << subjects["1A"].q1_code << endl;
         cout << subjects["1B"].q1_code << endl;
@@ -449,7 +450,7 @@ public:
         cout << subjects["1D"].q5_code << endl;
         cout << subjects["1E"].q5_code << endl;
         cout << subjects["1F"].q5_code << endl;
-        
+
         // SESSION 2 - responses
         cout << subjects["2A"].q1 << endl;
         cout << subjects["2B"].q1 << endl;
@@ -481,7 +482,7 @@ public:
         cout << subjects["2D"].q5 << endl;
         cout << subjects["2E"].q5 << endl;
         cout << subjects["2F"].q5 << endl;
-        
+
         // SESSION 2 - coding
         cout << subjects["2A"].q1_code << endl;
         cout << subjects["2B"].q1_code << endl;
@@ -513,19 +514,25 @@ public:
         cout << subjects["2D"].q5_code << endl;
         cout << subjects["2E"].q5_code << endl;
         cout << subjects["2F"].q5_code << endl;
-        
+
         return 0;
     } // print()
-    
+
     ////// Save Output Functions //////
-    
+
     int write_to_file() {
+        // Get current date/time and add to file name, so that every data file name is unique and never gets overwritten
+        time_t date_time;
+        time(&date_time);
+        string data_filename_pt1 = "data_";
+        string data_filename_pt2 = data_filename_pt1.append(ctime(&date_time));
+        string data_filename_pt3 = data_filename_pt2.append(".csv");
         ofstream data;
-        data.open("data.csv");
-        
+        data.open(data_filename_pt3);
+
         // Print header
         data << "SubjectID,Q1Response,Q2Response,Q3Response,Q4Response,Q5Response,CoderQ1,CoderQ2,CoderQ3,CoderQ4,CoderQ5,\n";
-        
+
         // Remove commas
         unordered_map<string, subject>::iterator clean = subjects.begin();
         while (clean != subjects.end()) {
@@ -536,7 +543,7 @@ public:
             (clean->second.q5).erase(remove(clean->second.q5.begin(), clean->second.q5.end(), ','), clean->second.q5.end());
             ++clean;
         }
-        
+
         // Print contents
         unordered_map<string, subject>::iterator it = subjects.begin();
         while (it != subjects.end()) {
@@ -553,7 +560,7 @@ public:
                               << "\n";
             ++it;
         }
-        
+
         data.close();
         return 0;
     } // write_to_file()
@@ -561,21 +568,21 @@ public:
 
 int main(int argc, const char * argv[]) {
     completeData DATA;
-    
+
     // Direct control flow to 'Functions to Store Responses'
     DATA.process_session();
-    
+
     // Direct control flow to 'User Interface Functions'
     DATA.make_response_list();
-    
+
     // Test storage
     // DATA.print();
-    
+
     // Direct control flow to 'Save Output Functions'
     DATA.write_to_file();
-    
+
     // Print closing statement
     cout << "\n\nThank you for using the CommandLineClassifier program. The file data.csv has been generated in your directory.\n";
-    
+
     return 0;
 } // main()
